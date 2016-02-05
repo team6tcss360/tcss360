290dde1fc7dfa50421e6d300ffc036236fe0e5a8
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-02-05 12:37:37 -0800

    update a little in staffmember

--- UrbanParks/StaffMember.java
+++ UrbanParks/StaffMember.java
@@ -9,30 +9,35 @@
  */
 public class StaffMember extends User {
 
-    /**
-     * Creates a StaffMember through the User constructor.
-     */
-    public StaffMember(String inputFirstName, String inputLastName, String inputEmail, String inputPhone,
-            int inputUserID) {
-        super(inputFirstName, inputLastName, inputEmail, inputPhone, inputUserID);
-    }
+	/**
+	 * Creates a StaffMember through the User constructor.
+	 */
+	public StaffMember(String inputFirstName, String inputLastName, String inputEmail, String inputPhone,
+			int inputUserID) {
+		super(inputFirstName, inputLastName, inputEmail, inputPhone, inputUserID);
+	}
 
-    /**
-     * Gets a list of volunteer last names. 
-     * 
-     * @return A list of volunteer last names as a String.
-     */
-    public String getVolunteerLastNames(UserList<User> inputList){
-    	
-    	StringBuilder lastNames = new StringBuilder();
-    	for(int i = 0; i< inputList.size(); i++){
-    		if(i==inputList.size()-1){
-    			lastNames.append(inputList.get(i).getLastName());
-    		}else{
-    			lastNames.append(inputList.get(i).getLastName()+", ");
-    		}	
-    	}
+	/**
+	 * Gets a list of volunteer last names. 
+	 * 
+	 * @return A list of volunteer last names as a String.
+	 */
+	public String getVolunteerLastNames(UserList<User> inputList){
+		StringBuilder lastNames = new StringBuilder();
+		if(inputList.size()==0){
+			lastNames.append("No users!");
+		}else{
+			for(int i = 0; i< inputList.size(); i++){
+				if(i==inputList.size()-1){
+					lastNames.append(inputList.get(i).getLastName());
+				}else{
+					lastNames.append(inputList.get(i).getLastName()+", ");
+				}	
+			}
+		}
+
+
 		return lastNames.toString();
-    	
-    }
+
+	}
 }
