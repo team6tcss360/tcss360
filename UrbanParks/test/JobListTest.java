06fc30c6fec0726ea77c9346ef9528379c406666
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-03-03 12:16:15 -0800

    All but 1 test completed

--- UrbanParks/test/JobListTest.java
+++ UrbanParks/test/JobListTest.java
@@ -38,7 +38,6 @@ public class JobListTest {
 	private Job j3;
 	private Job j4;
 	private Job j5;
-	private Job j6;
 	private Job j7;
 
 	private String badStart;
@@ -117,7 +116,6 @@ public class JobListTest {
 		j3 = new Job(3, "03-11-16 1:00PM", "03-12-16 4:00PM", "Dash Point", "The volunteers will help clean the beach.", 5, 0, 0);
 		j4 = new Job(4, "03-13-16 2:00PM", "03-13-16 4:00PM", "Point Defiance", "The volunteers will help pickup trash on the trails.", 5, 5, 0);
 		j5 = new Job(5, "03-22-16 2:00PM", "03-22-16 4:00PM", "South Park", "The volunteers will help clean the beach.", 3, 1, 1);
-		j6 = new Job(5, "03-22-16 2:00PM", "03-22-16 4:00PM", "South Park", "The volunteers will help clean the beach.", 3, 1, 1);
 		j7 = new Job(7, job1Start, job1End, "Mount Rainier", "Volunteers move stuff.", 5,2,3);
 //		jobArray= {j1, j2, j3, j4, j5};
 		pm1 = new ParkManager("Michael", "Ford", "fordm13@uw.edu", "9494125944");
@@ -214,18 +212,36 @@ public class JobListTest {
 		jobList1.add(j3);
 		jobList1.add(j4);
 		jobList1.add(j5);
+		
+		jobList2.add(j1);
+		jobList2.add(j2);
+		jobList2.add(j3);
+		jobList2.add(j4);
+		jobList2.add(j5);
+		
+		
 		pList1.add(park1);
-//		String sum = jobList1.getSummariesMyParks(pList1, pm1);
-//		System.out.println(sum);
-		fail("not yet implemented");//TODO
+		String sum = jobList1.getSummariesMyParks(pList1, pm1);
+		String sum2 = jobList2.getSummariesMyParks(pList1, pm1);
+		System.out.println(sum);
+		
+		assertEquals(sum, sum2);
+//		fail("not yet implemented");//TODO
 	}
 
 	/**
 	 * Test method for {@link model.JobList#getSummariesMyVolunteerJobs(model.Volunteer)}.
+	 * @throws ParseException 
 	 */
 	@Test
-	public void testGetSummariesMyVolunteerJobs() {
-		fail("Not yet implemented"); // TODO
+	public void testGetSummariesMyVolunteerJobs() throws ParseException {
+		
+		j1.addMedVolunteer(v1);
+		
+		jobList1.add(j1);
+		jobList2.add(j1);
+		String summaries = jobList1.getSummariesMyVolunteerJobs(v1);
+		assertEquals(summaries, jobList2.getSummariesMyVolunteerJobs(v1));
 	}
 
 
